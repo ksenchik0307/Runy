9de446f2b2823c0cae27da8d51f3028241fee370
Oksana <stervozska@mail.ru>
2023-11-22 22:29:20 +0300

    add strong

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,12 @@
 import { poz1, poz23, poz4, poz56, poz7, poz8, poz9 } from "./modules/dataCont";
 import { dataRuny } from "./modules/dataRuny";
+import { dataAsov } from "./modules/dataAsov";
 const arrCont = [poz1, poz23, poz23, poz4, poz56, poz56, poz7, poz8, poz9];
 
 const modal = document.getElementById("myModal");
 const btn = document.querySelectorAll(".item");
 const btnRuny = document.querySelectorAll(".row-item");
+const btnAsov = document.querySelectorAll('.item-asov');
 const span = document.getElementsByClassName("close")[0];
 
 btn.forEach((el) => {
@@ -13,7 +15,7 @@ btn.forEach((el) => {
     
     const selected = +el.dataset.num;
     document.querySelector('.modal-title').innerHTML = arrCont[selected][0].title;
-    document.querySelector('.modal-text').innerHTML = arrCont[selected][0].text;
+    document.querySelector('.modal-text').innerHTML = arrCont[selected][0].text;   
   };
 });
 
@@ -27,6 +29,17 @@ btnRuny.forEach((el) => {
    }
 })
 
+btnAsov.forEach((el) => {
+   el.onclick = function(){
+      modal.style.display = "block";
+
+      const selected3 = +el.dataset.asov;
+      document.querySelector('.modal-title').innerHTML = dataAsov[selected3].title;
+      document.querySelector('.modal-text').innerHTML = dataAsov[selected3].text;
+   }
+})
+
+
 span.onclick = function () {
   modal.style.display = "none";
 };
@@ -36,4 +49,3 @@ window.onclick = function (event) {
     modal.style.display = "none";
   }
 };
-
